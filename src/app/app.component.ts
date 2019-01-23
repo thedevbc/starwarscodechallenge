6b04e54832dedb853bdad3e4021dd3ec9e47fe2c
ben <ben@ksbdental.com>
2019-01-23 09:58:48 -0600

    basic functionality complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  movies = [
+    { title: 'Star Wars Episode IV - A New Hope', selected: false, enabled: true },
+    { title: 'Star Wars Episode V - The Empire Strikes Back', selected: false, enabled: true },
+    { title: 'Star Wars Episode VI - Return of the Jedi', selected: false, enabled: true },
+    { title: 'Star Wars Episode I - The Phantom Menace', selected: false, enabled: true },
+    { title: 'Star Wars Episode II - Attack of the Clones', selected: false, enabled: true },
+    { title: 'Star Wars Episode III - Revenge of the Sith', selected: false, enabled: true },
+    { title: 'Star Wars Episode VII - The Force Awakens', selected: false, enabled: true },
+    { title: 'Star Wars Episode IX', selected: false, enabled: false },
+  ]
+
+
+
+  onMovieClick(movie) {
+    console.log(movie);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { MatButtonModule, MatCheckboxModule, MatCardModule, MatListModule, MatFormFieldModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatListModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
